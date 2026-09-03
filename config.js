4d6a72f072b1509c8893c52e8a41aff17e9b6acb
andreevaa140901 <andreevaa.140901@yandex.ru>
2026-09-04 01:13:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// config.js - Конфигурация для RS-Detailing
-
+// config.js
 window.DB_CONFIG = {
     local: {
         key: 'rs_detailing_v24'
@@ -8,6 +7,3 @@ window.DB_CONFIG = {
         url: 'https://rs-detailing-app.ru/api.php'
     }
 };
-
-console.log('✅ Конфигурация загружена');
-console.log('📡 API URL:', window.DB_CONFIG.api.url);
